6ddfe169786edb2e6d9de61dd9abcccdcab6e7f7
jogendr2018s <jogendraruppa@gmail.com>
2024-03-12 18:22:22 +0530

    1st change after last

--- 1.js
+++ 1.js
@@ -3,8 +3,10 @@
 require("dotenv").config()
 var express = require('express'); // requre the express framework
 var app = express();
+const cors = require('cors');
 var fs = require('fs'); //require file system object
 
+app.use(cors());
 
 app.use(express.json());
 // Endpoint to Get a list of users
@@ -12,7 +14,7 @@ app.get('/', function(req, res){
     fs.readFile(__dirname + "/" + "csvjson (1).json", 'utf8', function(err, data){
         const jsonData = JSON.parse(data);
         var companies =jsonData.filter(element => {
-           return  element.problem_link == req.query.link
+           return   req.query.link.includes(element.problem_link)
         });
         console.log(companies)
         var ans = companies.map(element => element.company_name)
